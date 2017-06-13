ecb465d67a7e8fa2af45f3811244ad74db717112
Max Klenk <max@klenk.biz>
2017-06-13 22:43:00 +0200

    try flags

--- run.sh
+++ run.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env sh
 
+
+
+# normal
 mkdir _build
 cd _build
-cmake ..
+cmake .. -DOPT0:BOOL=ON
 cmake --build . && ./run
+cd ..
+rm -R _build
+
+# normal
+mkdir _build
+cd _build
+cmake .. -DOPT1:BOOL=ON
+cmake --build . && ./run
+cd ..
+rm -R _build
+
+# normal
+mkdir _build
+cd _build
+cmake .. -DOPT2:BOOL=ON
+cmake --build . && ./run
+cd ..
+rm -R _build
+
+# normal
+mkdir _build
+cd _build
+cmake .. -DOPT3:BOOL=ON
+cmake --build . && ./run
+cd ..
+rm -R _build
+
+# release
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#cmake --build . && ./run
