b02a4a53d4bf98f29b3bb827f21cf3cf81934cf8
Max Klenk <max@klenk.biz>
2017-06-14 06:26:46 +0200

    run with different configurations

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
-
-
 # normal
+mkdir _build
+cd _build
+cmake ..
+cmake --build . && ./run
+cd ..
+rm -R _build
+
+# O-flag -O0
 mkdir _build
 cd _build
 cmake .. -DOPT0:BOOL=ON
@@ -10,7 +16,7 @@ cmake --build . && ./run
 cd ..
 rm -R _build
 
-# normal
+# O-flag -O1
 mkdir _build
 cd _build
 cmake .. -DOPT1:BOOL=ON
@@ -18,7 +24,7 @@ cmake --build . && ./run
 cd ..
 rm -R _build
 
-# normal
+# O-flag -O2
 mkdir _build
 cd _build
 cmake .. -DOPT2:BOOL=ON
@@ -26,7 +32,7 @@ cmake --build . && ./run
 cd ..
 rm -R _build
 
-# normal
+# O-flag -O3
 mkdir _build
 cd _build
 cmake .. -DOPT3:BOOL=ON
@@ -34,6 +40,18 @@ cmake --build . && ./run
 cd ..
 rm -R _build
 
+# O-flag -O4
+mkdir _build
+cd _build
+cmake .. -DOPT4:BOOL=ON
+cmake --build . && ./run
+cd ..
+rm -R _build
+
 # release
-#cmake .. -DCMAKE_BUILD_TYPE=Release
-#cmake --build . && ./run
+mkdir _build
+cd _build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . && ./run
+cd ..
+rm -R _build
