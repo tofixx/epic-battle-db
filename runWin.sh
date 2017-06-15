9946b6132a446aa33f6e5b7a030e08f6947b0ea4
Max Klenk <max@klenk.biz>
2017-06-15 16:45:40 +0200

    format and add win configuration

--- runWin.sh
+++ runWin.sh
@@ -1,6 +1,57 @@
 #!/usr/bin/env sh
 
+# normal
 mkdir _build
 cd _build
 cmake ..
 cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# O-flag -O0
+mkdir _build
+cd _build
+cmake .. -DOPT0:BOOL=ON
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# O-flag -O1
+mkdir _build
+cd _build
+cmake .. -DOPT1:BOOL=ON
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# O-flag -O2
+mkdir _build
+cd _build
+cmake .. -DOPT2:BOOL=ON
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# O-flag -O3
+mkdir _build
+cd _build
+cmake .. -DOPT3:BOOL=ON
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# O-flag -O4
+mkdir _build
+cd _build
+cmake .. -DOPT4:BOOL=ON
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
+
+# release
+mkdir _build
+cd _build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . && Debug/run.exe
+cd ..
+rm -R _build
