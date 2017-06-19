d1758a7160c5cc18ba7985e991055bcc9f6285ad
Tobias Wollowski <tobias@jbbr.net>
2017-06-19 19:00:59 +0200

    benchmarking sample with writing results to file added for insertion

--- tests/insert.cpp
+++ tests/insert.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <assert.h>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
 
 #include "../sources/table.h"
 #include "../sources/columnStoreTable.h"
@@ -25,43 +27,58 @@ int32_t *getRandomValuesInRange(int32_t numValues, int32_t maxValue)
 void test_insert_row_table(int maxRows, int columns, int insertRows, bool printRow)
 {
     assert(insertRows <= maxRows);
-    std::cout << "insert " << insertRows << " rows in row table of size " << maxRows << std::endl;
+    std::cout << "insert " << insertRows << " rows in row table with " << columns << " columns of size " << maxRows << std::endl;
 
-    RowStoreTable t = RowStoreTable(maxRows, columns);
+    RowStoreTable *t = new RowStoreTable(maxRows, columns);
     for (int32_t i = 0; i < insertRows; ++i)
     {
         auto rowData = getRandomValuesInRange(columns, 300);
-        auto result = t.insert(rowData);
+        auto result = t->insert(rowData);
         // std::cout << "inserted in line " << result << std::endl;
         if (printRow)
         {
-            t.print_row(result);
+            t->print_row(result);
         }
         delete rowData;
     }
+    delete t;
 }
 
 void test_insert_column_table(int maxRows, int columns, int insertRows)
 {
     assert(insertRows <= maxRows);
-    std::cout << "insert " << insertRows << " rows in column table of size " << maxRows << std::endl;
+    std::cout << "insert " << insertRows << " rows in column table with " << columns << " columns of size " << maxRows << std::endl;
 
-    ColumnStoreTable t = ColumnStoreTable(maxRows, columns);
+    ColumnStoreTable *t = new ColumnStoreTable(maxRows, columns);
     for (int32_t i = 0; i < insertRows; ++i)
     {
         auto rowData = getRandomValuesInRange(columns, 300);
-        auto result = t.insert(rowData);
+        auto result = t->insert(rowData);
         // std::cout << "inserted in line " << result << std::endl;
         delete rowData;
     }
+    delete t;
 }
 
 int main(int argc, char const *argv[])
 {
-    TimeTimer<> timer = TimeTimer<>(10);
-   
+    TimeTimer<> timer = TimeTimer<>(3);
+
     timer.measure(test_insert_row_table, 10, 10, 10, true);
     timer.measure(test_insert_row_table, 120000, 100, 100000, false);
     timer.measure(test_insert_column_table, 120000, 100, 100000);
 
+    std::ofstream out("insert.csv");
+    out << "style,rows,columns,time ns" << std::endl;
+    for (int32_t rows = 10000; rows <= 1000000; rows *= 10)
+    {
+        for (int columns = 1; columns <= 128; columns *= 2)
+        {
+            auto time_insert_row_style = timer.measure(test_insert_row_table, rows, columns, rows, false);
+            auto time_insert_col_style = timer.measure(test_insert_column_table, rows, columns, rows);
+
+            out << "row-style," << rows << "," << columns << "," << time_insert_row_style << std::endl;
+            out << "col-style," << rows << "," << columns << "," << time_insert_col_style << std::endl;
+        }
+    }
 }
