b660235f7b9be02c6a6a9c03253d9e7bdd3e63d8
Jan <jan.kohstall@gmail.com>
2017-06-22 12:58:05 +0200

    Added a basic function to login to Hana. Somehow connection doesn't work yet. Credentials need to be written manually in config file.

---
+++ SAP-Hana-Scripts/config_to_be_copied.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+login = dict (
+	host = "",
+	port=,
+	user="",
+	password=""
+)
--- SAP-Hana-Scripts/createDB.py
+++ SAP-Hana-Scripts/createDB.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pyhdb
+import config
 
 # Creates the desired databases
+
+try:
+	connection = pyhdb.connect(
+		host=config.login["host"],
+		port=config.login["port"],
+		user=config.login["SYSTEM"],
+		password=config.login["manager"]
+	)
+#return connection
+except Exception as e:
+	print('Connection Failed!\nError Code is %s;\nError Content is %s;' % (e.args[0],e.args[1]))	
+
+cursor = connection.cursor()
+# cursor.execute("SELECT 'Hello Python World' FROM DUMMY")
+# cursor.fetchone()
+
+connection.close()
