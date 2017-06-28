0591536ed10d37b39c48f57fc75a2822d21d3e79
Judith Hartmann <judith.hartmann@student.hpi.de>
2017-06-28 10:14:49 +0200

    Add Update Function

--- sources/rowStoreTable.h
+++ sources/rowStoreTable.h
@@ -9,6 +9,7 @@ class RowStoreTable : public Table
     RowStoreTable(int32_t maxRows, int32_t maxColumns);
     virtual ~RowStoreTable() = default;
     int32_t insert_row(int32_t * values);
+    int32_t update(int32_t rowIndex, int32_t *values) override;
     int32_t &getLocation(const int32_t &row, const int32_t &column) override;
     Table *position_list_materialize(std::vector<int32_t> &positions, const int32_t columns, const int32_t *columnIds) override;
 };
