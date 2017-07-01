22f82a251a00a5f9004ab9901fec9c4db3b42b29
Jan <jan.kohstall@gmail.com>
2017-07-01 11:27:24 +0200

    Started building the threaded tests.

---
+++ tests/threads.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "../sources/table.h"
+#include "../sources/columnStoreTable.h"
+#include "../sources/rowStoreTable.h"
+#include "../sources/timer.h"
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* week  tests */
+    // make the tests threaded
+
+}
