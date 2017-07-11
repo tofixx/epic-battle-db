84d5b155b20f3e52bfb25a8ebf97380bb7f54d7d
Max Klenk <max@klenk.biz>
2017-07-11 10:45:27 +0200

    add vm-results

--- runBenchmarks.sh
+++ runBenchmarks.sh
@@ -10,5 +10,5 @@ cmake --build .
 ./benchmark_scan 10000000 100
 ./benchmark_update 1000000 100
 ./benchmark_updateLess 1000000 100
-./benchmark_thread 10000000 100 8
-./benchmark_thread 10000000 100 4
+./benchmark_thread 1000000 100 8
+./benchmark_thread 1000000 100 4
